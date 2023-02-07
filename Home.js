2ced43deaf15e6bd92045b41c3dbbb9a886ea63d
sid <siddheshmhatre.jobs@gmail.com>
2023-02-08 02:55:39 +0530

    added otp.js and validation in login form

--- Home.js
+++ Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text,Dimensions} from 'react-native';
 import Background from './Background';
 import Btn from './Btn';
 import { darkGreen, green } from './Constants';
 
+const {dheight,dwidth}=Dimensions.get('window')
 const Home = (props) => {
   return (
     <Background>
       <View style={{ marginHorizontal: 40, marginVertical: 100 }}>
       <Text style={{ color: 'white', fontSize: 64, marginBottom: 400 }}> </Text>
-      <Btn bgColor={'#c4ceab'} textColor='white' btnLabel="Login" Press={() => props.navigation.navigate("Login")} />
-      <Btn bgColor='white' textColor={darkGreen} btnLabel="Signup" Press={() => props.navigation.navigate("Signup")} />
+      <View >
+        <Btn   bgColor={'#c4ceab'} textColor='white' btnLabel="Login" Press={() => props.navigation.navigate("Login")} />
+        <Btn bgColor='white' textColor={darkGreen} btnLabel="Signup" Press={() => props.navigation.navigate("Signup")} />
+      </View>
       </View>
     </Background>
   );
