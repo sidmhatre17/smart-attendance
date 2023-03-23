3a60d39bb106c0008e80631f88de4f295e3e4cb9
sid <siddheshmhatre.jobs@gmail.com>
2023-03-23 18:31:53 +0530

    added face scanner

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './Home';
@@ -9,6 +12,9 @@ import H1 from './H1'
 import otp from './otp'
 import Final from './final'
 import Ic from './Ic'
+import FaceScan from './FaceScan'
+
+
 
 
 
@@ -25,6 +31,7 @@ function App() {
         <Stack.Screen name="Final" component={Final} />
         <Stack.Screen name="H1" component={H1} />
         <Stack.Screen name="otp" component={otp} />
+        <Stack.Screen name="FaceScan" component={FaceScan} />
         <Stack.Screen name="qr" component={qr} />
 
       </Stack.Navigator>
@@ -33,3 +40,15 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
